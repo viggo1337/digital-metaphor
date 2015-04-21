2832cd07a7f2b4cef5e9e27fbdf019fbb425ccc1
Martin Dilling-Hansen <mdh@intern1.dk>
2015-04-21 18:11:26 +0200

    Added index page
    The webroot will be pointing to the public directory

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Digital Metaphor</title>
+</head>
+<body>
+
+    <h1><a href="https://github.com/martindilling/digital-metaphor" target="_blank">Digital Metaphor</a></h1>
+
+    <p>
+        All (secure) pull requests are accepted!
+    </p>
+
+    Contributers: <a href="https://github.com/martindilling" target="_blank">Martin Dilling-Hansen</a>
+
+</body>
+</html>
+
+
