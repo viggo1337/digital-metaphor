355ea6c2a04c0d0ba6375af204f9f1c0f809ec36
C0DK <master@thecdk.net>
2015-04-23 23:08:28 +0200

    MOTHERFUCKING COMMIT!
    Commit to this. it's wild. it's dangerous. pee might actually come out.
    it's the commit that finally made this website fully 'WEB three point a
    million"!.
    
    Get ready. hold your socks.
    it's gonna be wild

--- public/index.php
+++ public/index.php
@@ -26,5 +26,18 @@ require __DIR__ . '/../app/framework.php';
 		Contributers: <a href="https://github.com/martindilling" target="_blank">Martin Dilling-Hansen</a>
 		Contributers: <a href="mailto:mauran@whydontyoufuck.me">Mauran Muthiah</a>
 
+		<br><br>
+		<h1>IT's MOTHERFUCKING GO TIME</h1>
+		This isn't just any site. it's a great a site <i>MADE BY THE COMMUNITY</i>. Why? BECAUSE WHY THE FUCK NOT!
+		It's a great site. it has everything. CSS (IN THE SAME FILE. EFFICIENCY BABY!), PHP, GRYBBES FREAKIn' FRAMEWORK.
+		AND, THIS SITE COULD DO ANYTHING. THE NEXT LINE MIGHT HAVE A FREAKIN LINK IN IT!.<br>
+		<a href="https://google.com">AND IT MAY BE MISLEADING!</a><br>
+		This page is so freaking wild, that right now, a freakin YOUTUBE video WILL appear!
+		<h3>DID YOU EXPECT TO FIND THIS WHEN YOU WOKE UP THIS MORNING?!</h3>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/6Kzvtc2HLWA" frameborder="0" allowfullscreen></iframe>
+
+		Welcome! To this freakin website.. let's hope it's still here tomorrow, but who knows? 
+		It's so freakin wild that it might be gone!. 
+		<h1>ENJOY!</h1>
 	</body>
 </html>
